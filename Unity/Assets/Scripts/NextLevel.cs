e7b0be7fe941e7df15fa51fd2ac527108feb5833
burakyalinat <burakyalinat@hotmail.com>
2021-08-11 23:36:46 +0300

    Overall Progress
    - Finish Line changed to Next Level
    - New level created to test the new script
    - An animation added to the enemy

---
+++ Unity/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NextLevel : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
